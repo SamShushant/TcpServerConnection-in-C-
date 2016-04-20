db8a0b5e58ee766a0f52a99960c03b888ec6e035
Sam <shushant@hotmail.es>
2016-04-20 02:20:10 +0200

    Create TcpServer.cs

---
+++ TcpServer.cs
@@ -0,0 +1,69 @@
+    public string valorQuant = "";
+    public int puerto = 8080;
+    public void ConexionTcp()
+        {
+            try
+            {
+                Thread tcpserverRunThread = new Thread(new ThreadStart(ProcesoAcceptarPeticionDelCliente));
+                tcpserverRunThread.Start();
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+        public void ProcesoAcceptarPeticionDelCliente()
+        {
+            try
+            {
+                tcpListener = new TcpListener(IPAddress.Any, puerto);
+                tcpListener.Start();
+                while (true)
+                {
+                    TcpClient client = tcpListener.AcceptTcpClient();
+                    Thread tcpHandlerThread = new Thread(new ParameterizedThreadStart(ProcesoRecibirMessage));
+                    tcpHandlerThread.Start(client);
+                }
+            }
+            catch (Exception)
+            {
+            }
+        }
+
+        public void ProcesoRecibirMessage(object client)
+        {
+            try
+            {
+                TcpClient mclient = (TcpClient)client;
+                NetworkStream stream = mclient.GetStream();
+                Byte[] message = new byte[1024];
+                stream.Read(message, 0, message.Length);
+                Message(Encoding.ASCII.GetString(message));
+                MessageBox.Show("Mensaje Recibido");
+                stream.Close();
+                mclient.Close();
+                tcpListener.Stop();
+            }
+            catch (Exception)
+            {
+            }
+        }
+        public string Message(string s)
+        {
+            try
+            {
+                Func<int> del = delegate ()
+                {
+                    valorQuant = s + System.Environment.NewLine;
+                    Messagebox.show(valorQuant);
+                    return 0;
+
+                };
+                Invoke(del);
+            }
+            catch (Exception)
+            {
+            }
+            return valorQuant;
+        }
+
